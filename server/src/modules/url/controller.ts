74ae14a3b73df5bd43fabad6fc5cf93aab415a12
SohamShirke473 <sohamshirke473@gmail.com>
2026-01-05 18:52:51 +0530

    feat: Implement full URL CRUD operations and add API routes documentation.

--- server/src/modules/url/controller.ts
+++ server/src/modules/url/controller.ts
@@ -3,6 +3,7 @@ import { nanoid } from "nanoid";
 import db from "../../db";
 import { url as urlTable } from "../../db/schema";
 import { AuthRequest } from "../auth/middleware";
+import { eq, and } from "drizzle-orm";
 
 export const shortenUrlHandler = async (req: AuthRequest, res: Response) => {
     try {
@@ -31,6 +32,86 @@ export const shortenUrlHandler = async (req: AuthRequest, res: Response) => {
         return res.status(201).json({
             message: "URL created successfully",
             data: newUrl,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const getUrlsHandler = async (req: AuthRequest, res: Response) => {
+    try {
+        const userId = req.userId;
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        const urls = await db.select().from(urlTable).where(eq(urlTable.user_id, userId));
+        return res.status(200).json({
+            message: "URLs retrieved successfully",
+            data: urls,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const getUrlbyIdHandler = async (req: AuthRequest, res: Response) => {
+    try {
+        const { id } = req.params;
+        const userId = req.userId;
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        const [url] = await db.select().from(urlTable).where(and(eq(urlTable.id, id), eq(urlTable.user_id, userId)));
+        if (!url) {
+            return res.status(404).json({ message: "URL not found" });
+        }
+        return res.status(200).json({
+            message: "URL retrieved successfully",
+            data: url,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const deleteUrlHandler = async (req: AuthRequest, res: Response) => {
+    try {
+        const { id } = req.params;
+        const userId = req.userId;
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        const [deletedUrl] = await db.delete(urlTable).where(and(eq(urlTable.id, id), eq(urlTable.user_id, userId))).returning();
+        if (!deletedUrl) {
+            return res.status(404).json({ message: "URL not found" });
+        }
+        return res.status(200).json({
+            message: "URL deleted successfully",
+            data: deletedUrl,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const updateUrlHandler = async (req: AuthRequest, res: Response) => {
+    try {
+        const { id } = req.params;
+        const userId = req.userId;
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        const [updatedUrl] = await db.update(urlTable).set({ url: req.body.url }).where(and(eq(urlTable.id, id), eq(urlTable.user_id, userId))).returning();
+        if (!updatedUrl) {
+            return res.status(404).json({ message: "URL not found" });
+        }
+        return res.status(200).json({
+            message: "URL updated successfully",
+            data: updatedUrl,
         });
     } catch (err) {
         console.error(err);
--- server/src/modules/url/routes.ts
+++ server/src/modules/url/routes.ts
@@ -1,30 +1,23 @@
 import { Router } from "express";
-import { shortenUrlHandler } from "./controller";
+import { shortenUrlHandler, getUrlsHandler, getUrlbyIdHandler, deleteUrlHandler, updateUrlHandler } from "./controller";
 
 const router = Router();
 
 //to make url shorten
 router.post("/url", shortenUrlHandler);
 
-//to get all shorten urls
-router.get("/urls", (req, res) => {
-    res.json({ message: "shorten" })
-});
+// to get all shorten urls
+router.get("/urls", getUrlsHandler);
 
 //to get shorten url by id
-router.get("/url/:id", (req, res) => {
-    res.json({ message: "shorten" })
-});
+router.get("/url/:id", getUrlbyIdHandler)
 
 //to delete shorten url by id
-router.delete("/url/:id", (req, res) => {
-    res.json({ message: "shorten" })
-});
+router.delete("/url/:id", deleteUrlHandler)
 
 //to update shorten url by id
-router.put("/url/:id", (req, res) => {
-    res.json({ message: "shorten" })
-});
+router.put("/url/:id", updateUrlHandler)
+
 
 
 export default router;
