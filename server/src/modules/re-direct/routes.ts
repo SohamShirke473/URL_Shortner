3ba42890b29960efbda05b2332e0d36c20690168
SohamShirke473 <sohamshirke473@gmail.com>
2026-01-05 19:25:44 +0530

    feat: Add Redis caching for URL redirection, include Redis service in docker-compose, and document API routes.

--- server/src/modules/re-direct/routes.ts
+++ server/src/modules/re-direct/routes.ts
@@ -2,7 +2,20 @@ import { Router } from "express";
 import db from "../../db";
 import { url as urlTable } from "../../db/schema";
 import { eq } from "drizzle-orm";
+import Redis from "ioredis";
 
+export const redis = new Redis({
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASSWORD,
+});
+redis.on("connect", () => {
+    console.log("✅ Redis connected");
+});
+
+redis.on("error", (err) => {
+    console.error("❌ Redis error", err);
+});
 const router = Router();
 
 router.get("/:shortCode", async (req, res) => {
@@ -11,11 +24,17 @@ router.get("/:shortCode", async (req, res) => {
         if (!shortCode) {
             return res.status(400).json({ message: "Short code is required" });
         }
+        const cacheKey = `short:${shortCode}`;
+        const cachedUrl = await redis.get(cacheKey);
+        if (cachedUrl) {
+            return res.status(302).redirect(cachedUrl);
+        }
         const [url] = await db.select().from(urlTable).where(eq(urlTable.short_code, shortCode));
         if (!url) {
             return res.status(404).json({ message: "URL not found" });
         }
-        return res.redirect(url.url);
+        await redis.set(cacheKey, url.url, "EX", 60 * 60);
+        return res.status(302).redirect(url.url);
     } catch (err) {
         console.error(err);
         return res.status(500).json({ message: "Internal server error" });
